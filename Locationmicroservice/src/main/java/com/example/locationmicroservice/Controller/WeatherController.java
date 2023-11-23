8d8196a228744551dee9bf10b6776a39445540f6
viingar <myuniverse8082@gmail.com>
2023-11-23 23:04:02 +0300

    5.10 Задание 23. Балансировщик нагрузки.

--- Locationmicroservice/src/main/java/com/example/locationmicroservice/Controller/WeatherController.java
+++ Locationmicroservice/src/main/java/com/example/locationmicroservice/Controller/WeatherController.java
@@ -4,6 +4,7 @@ import com.example.locationmicroservice.Model.Geodata;
 import com.example.locationmicroservice.Model.Weather;
 import com.example.locationmicroservice.Repository.GeodataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,13 +17,16 @@ public class WeatherController {
     private GeodataRepository repository;
     @Autowired
     private RestTemplate restTemplate;
+    @Value("${weather.url}")
+    String weatherUrl;
 
     @GetMapping("/weather")
     public Weather redirectRequestWeather(@RequestParam String location) {
         Geodata geodata = repository.findByName(location).get();
-        String url = String.format("http://localhost:8070/?lat=%s&lon=%s", geodata.getLat(), geodata.getLon());
+        String url = String.format("http://%s/?lat=%s&lon=%s", weatherUrl, geodata.getLat(), geodata.getLon());
         return restTemplate.getForObject(url, Weather.class);
     }
+
 
     @GetMapping
     public Optional<Geodata> getWeather(@RequestParam String location) {
